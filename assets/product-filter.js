5e2d0368df43ff74b6419ef391a4566f68039350
Joel Glovier <jglovier@github.com>
2015-01-29 15:12:42 -0500

    add JS that makes it work

---
+++ assets/product-filter.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+  // Filter products by size
+  // --------------------------------------
+
+  $('.size-filter-option').on('click', function(event){
+    event.preventDefault();
+
+    // Handle the filter nav buttons -------------->
+
+    // remove not filtering indicator class if present, and add filtering indicator class
+    $(this).removeClass('js-not-filtering').addClass('js-current-filter');
+
+    // add not filtering class to rest of buttons
+    $(this).siblings('.size-filter-option').removeClass('js-current-filter').addClass('js-not-filtering');
+
+    // Handle the products to filter -------------->
+
+    var filteringBy = $(this).text();
+    filteringBy = "size-" + filteringBy;
+
+    if(filteringBy == 'size-All') {
+      $('.js-size-filter').removeClass('hidden');
+    } else {
+      $('.js-size-filter').each(function() {
+        if(!$(this).hasClass(filteringBy)) {
+          $(this).addClass('hidden');
+        } else {
+          $(this).removeClass('hidden');
+          $(this).addClass('active-filter');
+        }
+      });
+    }
+
+  });
+
+})
